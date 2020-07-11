9a0e8e65acaf630672dbd82f0249b701222460b7
万兴龙 <710121522@qq.com>
2020-07-11 14:35:38 +0800

    ‘上传文件’

--- bin/udpTest.js
+++ bin/udpTest.js
@@ -1,18 +1,4 @@
-#! /usr/bin/env node
 const udp=require('dgram');
-const { program } = require('commander');
-
-program
-  .option('-s, --server', 'start Server')
-  .option('-c, --client', 'start Client')
-  .option('-p, --port <type>', 'set port')
-  .option('-ip, --ip <type>', 'set ip')
-  .option('-id, --id <type>', 'set send id')
-  .option('-np, --numPacket <type>', 'set numPacket','1')
-  .option('-sc, --sendCount <type>', 'set sendCount','100')
-  .option('-pc, --perCount <type>', 'set perCount','1')
-  .option('-itl, --interval <type>', 'set interval','100')
-  .option('-st, --socketType <type>', 'set SocketType ipv4=udp4;ipv6=udp6','udp4');
 
 //返回变量类型
 function getType(obj){
@@ -619,16 +605,3 @@ module.exports=udpTest;
 //new udpTest(null,null,null,null,null,51232,false,null,'udp4');//启动服务端，端口为--51232
 //new udpTest(63,10,2,1000,'192.168.1.102',51232,true,41235,'udp4').sendData(1);//启动客户端，端口为--41235
 //new udpTest(10,10,2,1000,'192.168.1.102',51232,true,41235).sendData(2);//启动客户端，端口为--41235
-program.parse(process.argv);
-//console.log(program.opts())
-if (program.server&&program.port&&program.socketType) {
-    new udpTest(null,null,null,null,null,program.port,false,null,program.socketType);
-    console.log('udp服务端已成功运行，端口号'+program.port);
-}
-if (program.client&&program.port&&program.numPacket&&program.sendCount&&program.perCount&&program.interval
-    &&program.ip&&program.socketType&&program.id)  {
-    new udpTest(program.numPacket,program.sendCount,program.perCount,program.interval,program.ip,program.port,true,null,program.socketType).sendData(program.id)
-}
-if (!program.client&&!program.server)  {
-    program.outputHelp();
-}
